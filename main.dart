cdfdf02fa27a8fdcf86582e19aa1f9f5682d3907
Ezequiel Alarcon <138638611+EzeAlarcon@users.noreply.github.com>
2023-07-27 20:17:45 -0500

    Add files via upload

---
+++ main.dart
@@ -0,0 +1,68 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:restaurant/domain/bloc/blocs.dart';
+import 'package:restaurant/domain/services/push_notification.dart';
+import 'package:restaurant/presentation/screens/intro/checking_login_screen.dart';
+ 
+PushNotification pushNotification = PushNotification();
+
+Future<void> _firebaseMessagingBackground( RemoteMessage message ) async {
+
+  await Firebase.initializeApp();
+
+}
+
+void main() async {
+
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  FirebaseMessaging.onBackgroundMessage(_firebaseMessagingBackground);
+  pushNotification.initNotifacion();
+  runApp(MyApp());
+}
+ 
+
+ 
+class MyApp extends StatefulWidget {
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+
+  @override
+  void initState() {
+    pushNotification.onMessagingListener();
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    
+    SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle(statusBarColor: Colors.transparent, statusBarIconBrightness: Brightness.dark ));
+
+    return MultiBlocProvider(
+      providers: [ 
+        BlocProvider(create: (context) => AuthBloc()..add(CheckLoginEvent())),
+        BlocProvider(create: (context) => GeneralBloc()),
+        BlocProvider(create: (context) => ProductsBloc()),
+        BlocProvider(create: (context) => CartBloc()),
+        BlocProvider(create: (context) => UserBloc()),
+        BlocProvider(create: (context) => MylocationmapBloc()),
+        BlocProvider(create: (context) => PaymentsBloc()),
+        BlocProvider(create: (context) => OrdersBloc()),
+        BlocProvider(create: (context) => DeliveryBloc()),
+        BlocProvider(create: (context) => MapdeliveryBloc()),
+        BlocProvider(create: (context) => MapclientBloc()),
+      ],
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        title: 'Delivery-App',
+        home: CheckingLoginScreen(),
+      ),
+    );
+  }
+}
